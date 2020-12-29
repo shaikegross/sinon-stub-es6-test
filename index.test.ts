e82675c118ebba043dfc3293ce4f228893a9448e
Shai <shai.gross@meliopayments.com>
2020-12-29 10:56:38 +0200

    added another test and debug config

--- index.test.ts
+++ index.test.ts
@@ -1,25 +1,49 @@
 import * as greeter from './greeter';
 
+import { SinonStub, stub } from 'sinon';
+
 // Only module import in the entire project
 import { describe } from 'mocha';
 import { expect } from 'chai';
 import { greetWithTime } from './index';
-import { stub } from 'sinon';
 
 describe('stubbing a function that used a destructed import', () => {
+    let greetingWithTimeStub: SinonStub;
+
+    beforeEach(() => {
+        greetingWithTimeStub?.restore();
+    })
+
     it('should not stub', () => {
         const greetingWithTime = greetWithTime('shai');
         expect(greetingWithTime).to.satisfy((greeting: string) => greeting.startsWith('Hello '));
     });
-    
+
     it('should stub well', () => {
-        stub(greeter, 'createGreeting').callsFake((name: string) => {
+        greetingWithTimeStub = stub(greeter, 'createGreeting');
+
+        greetingWithTimeStub.callsFake((name: string) => {
             return `I have stubbed you ${name}`
         });
-        
-      
+
         const greetingWithTime = greetWithTime('shai');
+
         expect(greetingWithTime).to.satisfy((greeting: string) => greeting.startsWith('I have stubbed you'));
+    });
+
+    it('should stub well after calling it once unstubbed', () => {
+        const greetingWithTime = greetWithTime('shai');
+
+        greetingWithTimeStub = stub(greeter, 'createGreeting');
+
+        greetingWithTimeStub.callsFake((name: string) => {
+            return `I have stubbed you ${name}`
+        });
+
+        const greetingWithTimeStubbed = greetWithTime('shai');
+
+        expect(greetingWithTime).to.satisfy((greeting: string) => greeting.startsWith('Hello '));
+        expect(greetingWithTimeStubbed).to.satisfy((greeting: string) => greeting.startsWith('I have stubbed you'));
     });    
 });
 
